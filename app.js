2c63d5d5a5acf2a46bb4313cce40d2fe81b4b1a9
Mehmet Ali Peker <maps6134@hotmail.com>
2018-08-31 01:16:20 +0300

    2

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    console.log("Request: " + req.pathname);
+
+    trends('turkey', 20, function(err, data) {
+        if(err) {
+            console.log(err);
+            res.json({ status: false, message: 'technicalError' });
+        }
+
+        res.json({ status: true, objects: data });
+        res.end();
+    });
+});
+
 app.get('/:country/:count', function(req, res){
     const { country, count } = req.params;
 
